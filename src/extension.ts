f84a3696b8d4763e25fb418d970f6ad45a2115fd
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2023-01-22 14:56:40 +0000

    fix: :bug: Bundle manually
    Browser compatible extension cannot use import/require

--- src/extension.ts
+++ src/extension.ts
@@ -8,8 +8,6 @@ import {
     SnippetString,
 } from "vscode";
 
-import { pop3, smtp } from "./keywords.json";
-
 interface KeywordInfo {
     label: string;
     text?: string;
@@ -52,3 +50,110 @@ export function activate(context: ExtensionContext) {
 }
 
 export function deactivate() {}
+
+const pop3 = [
+    { text: "USER ", label: "USER", docs: "Login username" },
+    { text: "PASS ", label: "PASS", docs: "Login password" },
+    { text: "QUIT", label: "QUIT", docs: "Quit session" },
+    {
+        text: "STAT",
+        label: "STAT",
+        docs: "Statistic of messages",
+    },
+    { text: "LIST", label: "LIST", docs: "List messages" },
+    {
+        text: "RETR ",
+        label: "RETR",
+        docs: "Retrieve the whole messages",
+    },
+    {
+        text: "DELE ",
+        label: "DELE",
+        docs: "Delete the message",
+    },
+    {
+        text: "TOP ${1:index} ${2:lines}",
+        label: "TOP",
+        docs: "Header and Top few lines of message",
+    },
+    {
+        text: "UIDL ",
+        label: "UIDL",
+        docs: "Unique ID of a message",
+    },
+    { text: "NOOP", label: "NOOP", docs: "No Operation" },
+    {
+        text: "RSET",
+        label: "RSET",
+        docs: "Undelete messages that marked for deletion",
+    },
+];
+
+const smtp = [
+    {
+        text: "HELO ",
+        label: "HELO",
+        docs: "It provides the identification of the sender.",
+    },
+    {
+        text: "MAIL FROM: ",
+        label: "MAIL",
+        docs: "It specifies the originator of the mail.",
+    },
+    {
+        text: "RCPT TO: ",
+        label: "RCPT",
+        docs: "It specifies the recipient of mail.",
+    },
+    {
+        text: "DATA",
+        label: "DATA",
+        docs: "It specifies the beginning of the mail.",
+    },
+    {
+        text: "QUIT",
+        label: "QUIT",
+        docs: "It closes the TCP connection.",
+    },
+    {
+        text: "RSET",
+        label: "RSET",
+        docs: "It aborts the current mail transaction but the TCP connection remains open.",
+    },
+    {
+        text: "VRFY ",
+        label: "VRFY",
+        docs: "It is use to confirm or verify the user name.",
+    },
+    { text: "NOOP", label: "NOOP", docs: "No operation" },
+    {
+        text: "TURN",
+        label: "TURN",
+        docs: "It reverses the role of sender and receiver.",
+    },
+    {
+        text: "EXPN ",
+        label: "EXPN",
+        docs: "It specifies the mailing list to be expanded.",
+    },
+    {
+        text: "HELP",
+        label: "HELP",
+        docs: "It send some specific docs to the system.",
+    },
+    {
+        text: "SEND FROM: ",
+        label: "SEND",
+        docs: "It send mail to the terminal.",
+    },
+    {
+        text: "SOML FROM: ",
+        label: "SOML",
+        docs: "It send mail to the terminal if possible; otherwise to mailbox.",
+    },
+    {
+        text: "SAML FROM: ",
+        label: "SAML",
+        docs: "It send mail to the terminal and mailbox.",
+    },
+];
